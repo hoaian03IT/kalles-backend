fd64d1424418bec311bbe3cdf09bb86932a09c60
hoaian03 <dangtranhoaian8603@gmail.com>
2024-09-25 11:44:13 +0700

    integrate vnpay

--- src/apps/controllers/order.js
+++ src/apps/controllers/order.js
@@ -1,5 +1,22 @@
 const mongoose = require("mongoose");
-const { OrderModel, OrderLineModel } = require("../models");
+const { OrderModel } = require("../models");
+const moment = require("moment");
+
+function sortObject(obj) {
+    let sorted = {};
+    let str = [];
+    let key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            str.push(encodeURIComponent(key));
+        }
+    }
+    str.sort();
+    for (key = 0; key < str.length; key++) {
+        sorted[str[key]] = encodeURIComponent(obj[str[key]]).replace(/%20/g, "+");
+    }
+    return sorted;
+}
 
 class Order {
     async createOrder(req, res) {
@@ -121,6 +138,67 @@ class Order {
                 { id: 1, name: "Quick Delivery", fee: quickDeliveryFee },
                 { id: 2, name: "Express Delivery", fee: expressDeliveryFee },
             ]); // đơn vị: $
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    }
+
+    async createPayment(req, res, next) {
+        try {
+            process.env.TZ = "Asia/Ho_Chi_Minh";
+
+            let date = new Date();
+            let createDate = moment(date).format("YYYYMMDDHHmmss");
+
+            let ipAddr =
+                req.headers["x-forwarded-for"] ||
+                req.connection.remoteAddress ||
+                req.socket.remoteAddress ||
+                req.connection.socket.remoteAddress;
+
+            let config = require("config");
+
+            let tmnCode = config.get("vnp_TmnCode");
+            let secretKey = config.get("vnp_HashSecret");
+            let vnpUrl = config.get("vnp_Url");
+            let returnUrl = config.get("vnp_ReturnUrl") || req.body.returnUrl;
+            let orderId = moment(date).format("DDHHmmss");
+            let amount = req.body.amount;
+            let bankCode = req.body.bankCode;
+
+            let locale = req.body.language;
+            if (locale === null || locale === "") {
+                locale = "vn";
+            }
+            let currCode = "VND";
+            let vnp_Params = {};
+            vnp_Params["vnp_Version"] = "2.1.0";
+            vnp_Params["vnp_Command"] = "pay";
+            vnp_Params["vnp_TmnCode"] = tmnCode;
+            vnp_Params["vnp_Locale"] = locale;
+            vnp_Params["vnp_CurrCode"] = currCode;
+            vnp_Params["vnp_TxnRef"] = orderId;
+            vnp_Params["vnp_OrderInfo"] = "Thanh toan cho ma GD:" + orderId;
+            vnp_Params["vnp_OrderType"] = "other";
+            vnp_Params["vnp_Amount"] = amount * 100;
+            vnp_Params["vnp_ReturnUrl"] = returnUrl;
+            vnp_Params["vnp_IpAddr"] = ipAddr;
+            vnp_Params["vnp_CreateDate"] = createDate;
+            if (bankCode !== null && bankCode !== "") {
+                vnp_Params["vnp_BankCode"] = bankCode;
+            }
+
+            vnp_Params = sortObject(vnp_Params);
+
+            let querystring = require("qs");
+            let signData = querystring.stringify(vnp_Params, { encode: false });
+            let crypto = require("crypto");
+            let hmac = crypto.createHmac("sha512", secretKey);
+            let signed = hmac.update(new Buffer(signData, "utf-8")).digest("hex");
+            vnp_Params["vnp_SecureHash"] = signed;
+            vnpUrl += "?" + querystring.stringify(vnp_Params, { encode: false });
+
+            res.status(200).json({ url: vnpUrl });
         } catch (error) {
             res.status(500).json({ message: error.message });
         }
--- src/router/order.router.js
+++ src/router/order.router.js
@@ -6,5 +6,6 @@ router.get("/detail/:id", authenticate, orderController.getDetailedOrder);
 router.post("/create", authenticate, orderController.createOrder);
 router.get("/all", authenticate, orderController.getOrders);
 router.get("/shipping-cost", orderController.getShipCost);
+router.post("/create-payment", orderController.createPayment);
 
 module.exports = router;
